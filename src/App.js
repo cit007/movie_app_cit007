5661ab4ef14c73553f23ccb8eade3c6297120744
cit007 <itcho0077@gmail.com>
2020-10-04 20:37:51 +0900

    npm i prop-types and check prototype

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Gallery({ id, name, image }) {
+function Gallery({ id, name, image, rating }) {
   return (
     <div>
       <h1>The Gallery Name is {name}</h1>
-      <h1>The Gallery Id is {id}</h1>
+      <h2>The Gallery Id is {id}</h2>
+      <h3>Rating is {rating}/5.0</h3>
       <img src={image} alt={name} />
     </div>
   );
 }
+
+Gallery.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
 
 const galleryArr = [
   {
@@ -16,18 +25,21 @@ const galleryArr = [
     name: "ishii cho sayaney #1",
     img_url:
       "https://www.sayaney.com/wp-content/uploads/2020/09/0008-701x1024-640x480.jpg",
+    rating: 4.2,
   },
   {
     id: 2,
     name: "ishii cho sayaney #2",
     img_url:
       "https://www.sayaney.com/wp-content/uploads/2020/09/0033-701x1024-640x480.jpg",
+    rating: 4.6,
   },
   {
     id: 3,
     name: "ishii cho sayaney #3",
     img_url:
       "https://www.sayaney.com/wp-content/uploads/2020/09/0032-701x1024-640x480.jpg",
+    rating: 4.7,
   },
 ];
 
@@ -42,6 +54,7 @@ function App() {
             id={glyObj.id}
             name={glyObj.name}
             image={glyObj.img_url}
+            rating={glyObj.rating}
           />
         );
       })}
