1c5814dbbdee95d2d87009c2dbdc6c34f06c9b21
cit007 <itcho0077@gmail.com>
2020-10-05 23:15:43 +0900

    npm i axios for fetch data

--- src/App.js
+++ src/App.js
@@ -1,59 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
     isLoading: true,
     movie: [],
-    count: 0,
   };
-
-  add = () => {
-    console.log("add");
-    // this.setState({ count: this.state.count + 1 });
-    // this.setState((current) => ({ count: this.state.count + 1 }));
-    this.setState((current) => ({ count: current.count + 1 }));
-  };
-
-  minus = () => {
-    console.log("minus");
-    // this.setState({ count: this.state.count - 1 });
-    // this.setState((current) => ({ count: this.state.count - 1 }));
-    this.setState((current) => ({ count: current.count + 1 }));
-  };
-
-  constructor(props) {
-    super(props);
-    console.log("consturctor");
-  }
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 3000);
-
-    console.log("did mount");
+    axios.get("https://yts-proxy.now.sh/list_movies.json");
   }
 
-  componentDidUpdate() {
-    console.log("did update");
-  }
-
-  componentWillUnmount() {
-    console.log("will unmount");
-  }
   render() {
-    console.log("render");
-    // const isLoading = this.state.isLoading;
     const { isLoading } = this.state;
-    return (
-      <div>
-        {isLoading ? "Loading...." : "We are ready"}
-        <h1>the number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minux</button>
-      </div>
-    );
+    return <div>{isLoading ? "Loading...." : "We are ready"}</div>;
   }
 }
 
