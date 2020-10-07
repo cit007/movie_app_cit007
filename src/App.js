8fb78cbab124f765c9cc62fa009add40ac19103b
cit007 <itcho0077@gmail.com>
2020-10-08 05:16:45 +0900

    merge [movie_css] - complete movie app

--- src/App.js
+++ src/App.js
@@ -1,65 +1,56 @@
 import React from "react";
-import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
-function Gallery({ id, name, image, rating }) {
-  return (
-    <div>
-      <h1>The Gallery Name is {name}</h1>
-      <h2>The Gallery Id is {id}</h2>
-      <h3>Rating is {rating}/5.0</h3>
-      <img src={image} alt={name} />
-    </div>
-  );
-}
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
 
-Gallery.propTypes = {
-  id: PropTypes.number.isRequired,
-  name: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-};
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    // console.log(movies);
 
-const galleryArr = [
-  {
-    id: 1,
-    name: "ishii cho sayaney #1",
-    img_url:
-      "https://www.sayaney.com/wp-content/uploads/2020/09/0008-701x1024-640x480.jpg",
-    rating: 4.2,
-  },
-  {
-    id: 2,
-    name: "ishii cho sayaney #2",
-    img_url:
-      "https://www.sayaney.com/wp-content/uploads/2020/09/0033-701x1024-640x480.jpg",
-    rating: 4.6,
-  },
-  {
-    id: 3,
-    name: "ishii cho sayaney #3",
-    img_url:
-      "https://www.sayaney.com/wp-content/uploads/2020/09/0032-701x1024-640x480.jpg",
-    rating: 4.7,
-  },
-];
+    this.setState({ isLoading: false, movies: movies });
+  };
 
-function App() {
-  return (
-    <div>
-      <h1>Hello World!!</h1>
-      {galleryArr.map((glyObj) => {
-        return (
-          <Gallery
-            key={glyObj.id}
-            id={glyObj.id}
-            name={glyObj.name}
-            image={glyObj.img_url}
-            rating={glyObj.rating}
-          />
-        );
-      })}
-    </div>
-  );
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader_text"></span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              ></Movie>
+            ))}
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Movie.js
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Movie.css";
+
+function Movie({ year, title, summary, poster, genres }) {
+  return (
+    <div className="movie">
+      <img src={poster} alt={title} title={title} />
+      <div className="movie_data">
+        <h3 className="movie_title">{title}</h3>
+        <h4 className="movie_year">{year}</h4>
+        <ul className="genres">
+          {genres.map((genre) => (
+            <li className="genres_genre">{genre}</li>
+          ))}
+        </ul>
+        <h4 className="movie_summary">{summary}</h4>
+      </div>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Movie;
