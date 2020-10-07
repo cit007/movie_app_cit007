54a0aa47d842b9bb16ac7c908ff4224d4ff8e925
cit007 <itcho0077@gmail.com>
2020-10-08 05:38:09 +0900

    Updates

---
+++ precache-manifest.c14ec4ef728f5af2a49fba69f6c13e09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a749d9c9b7d58cf1537f60b71330566",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0af91a306d29b4b68e6c",
+    "url": "/static/css/main.e86cbedf.chunk.css"
+  },
+  {
+    "revision": "a5d6820ed7c2ad4b87af",
+    "url": "/static/js/2.60e9744d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.60e9744d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0af91a306d29b4b68e6c",
+    "url": "/static/js/main.cd3cb882.chunk.js"
+  },
+  {
+    "revision": "a61df54af1fbea79a524",
+    "url": "/static/js/runtime-main.b817d9d7.js"
+  }
+]);
