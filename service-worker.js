d0e2eb70ea40d921abafba7e414918311f15e1b2
cit007 <itcho0077@gmail.com>
2020-10-08 13:17:49 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3c096bec1f237df31d971f3b5320e124.js"
+  "/precache-manifest.5839cb1e204c0606b10a438d1c87fa33.js"
 );
 
 self.addEventListener('message', (event) => {
