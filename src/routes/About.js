6e61f3be80fbea03e046fb43188fcdf4ee32e6b4
cit007 <itcho0077@gmail.com>
2020-10-09 04:15:19 +0900

    etc better view

--- src/routes/About.js
+++ src/routes/About.js
@@ -2,7 +2,20 @@ import React from "react";
 import "./About.css";
 
 function About() {
-  return <div className="about_data">About Page</div>;
+  return (
+    <div className="about_data">
+      <div>
+        <h2>Nice to meet you!</h2>
+        <a href="https://github.com/cit007/movie_app_cit007">
+          >> GITHUB SOURCE
+        </a>
+        <br></br>
+        <br></br>
+        <span> This is Ishii Cho Sayaney's Illustration</span>
+        <img src="https://www.sayaney.com/wp-content/themes/clean-journal/images/gallery/slider2-1920x800.jpg" />
+      </div>
+    </div>
+  );
 }
 
 export default About;
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Detail.css";
 
 class Detail extends React.Component {
   componentDidMount() {
@@ -13,10 +14,22 @@ class Detail extends React.Component {
       return null;
     }
     return (
-      <div>
-        <h3 className="detail_title">{location.state.title}</h3>
-        <img src={location.state.image} />
-        <h3 className="detail_summary">{location.state.summary}</h3>
+      <div className="detail_container">
+        <div>
+          <img src={location.state.image} />
+        </div>
+        <div className="detail_contents">
+          <h3 className="detail_title">{location.state.title}</h3>
+          <h3 className="detail_title">{location.state.year}</h3>
+          <h3 className="detail_title">
+            {location.state.genres.map((genre, index) => (
+              <li key={index} className="genres_genre">
+                {genre}
+              </li>
+            ))}
+          </h3>
+          <h3 className="detail_summary">{location.state.summary}</h3>
+        </div>
       </div>
     );
   }
