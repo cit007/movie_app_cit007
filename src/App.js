54bc2da5f6f8ddbf1cb6729cd99e5d437655c9ad
cit007 <itcho0077@gmail.com>
2020-10-08 13:42:00 +0900

    bug fix miss spell

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <div>
+      <div className="container">
         {isLoading ? (
           <div className="loader">
             <span className="loader_text"></span>
@@ -39,12 +39,13 @@ class App extends React.Component {
             {movies.map((movie) => (
               <Movie
                 key={movie.id}
+                id={movie.id}
                 year={movie.year}
                 title={movie.title}
                 summary={movie.summary}
                 poster={movie.medium_cover_image}
                 genres={movie.genres}
-              ></Movie>
+              />
             ))}
           </div>
         )}
