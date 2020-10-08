23f222bcb4c14ca9ad415174f8793ddefdde3db7
cit007 <itcho0077@gmail.com>
2020-10-09 03:12:34 +0900

    redirect unknown page to home

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -8,6 +8,10 @@ class Detail extends React.Component {
   render() {
     const { location, history } = this.props;
     console.log(this);
+    if (location.state == null) {
+      history.push("/"); //redirect home
+      return null;
+    }
     return (
       <div>
         <h3 className="detail_title">{location.state.title}</h3>
