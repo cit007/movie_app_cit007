e4f906bd3033a93cf2603be1b8b440df3bdec019
cit007 <itcho0077@gmail.com>
2020-10-09 12:32:47 +0900

    Updates

--- precache-manifest.8e0ea1e6353b73936909c58a20b59170.js
+++ precache-manifest.8e0ea1e6353b73936909c58a20b59170.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1cfc5f0afa8524cf69c7c4946f59ccc6",
+    "revision": "5f90582ab8252172789def799177676e",
     "url": "/movie_app_cit007/index.html"
   },
   {
-    "revision": "b81a7313a04281e6e337",
+    "revision": "3cababa6216f98914e51",
     "url": "/movie_app_cit007/static/css/main.8efe2c65.chunk.css"
   },
   {
-    "revision": "f6ffba37514e7d7add2c",
-    "url": "/movie_app_cit007/static/js/2.b7921430.chunk.js"
+    "revision": "4020b5989904dc7897c9",
+    "url": "/movie_app_cit007/static/js/2.24ebc551.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/movie_app_cit007/static/js/2.b7921430.chunk.js.LICENSE.txt"
+    "url": "/movie_app_cit007/static/js/2.24ebc551.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b81a7313a04281e6e337",
-    "url": "/movie_app_cit007/static/js/main.82699775.chunk.js"
+    "revision": "3cababa6216f98914e51",
+    "url": "/movie_app_cit007/static/js/main.1d9b3aa4.chunk.js"
   },
   {
     "revision": "2e55b4d3f3a026224ec6",
